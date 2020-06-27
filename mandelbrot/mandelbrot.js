af033aa5570ce76874e4c081e940254616fab6fa
nicolas42 <nicolas42@gmail.com>
2020-06-27 21:18:57 +1000

    mandelbrot cleanup

--- mandelbrot/mandelbrot.js
+++ mandelbrot/mandelbrot.js
@@ -1,67 +1,83 @@
 
-function hslToRgb(h, s, l) {
-	var r;
-	var g;
-	var b;
-	function hue2rgb(p, q, t) {
-		if (t < 0) {
-			t += 1;
-		}
-		if (t > 1) {
-			t -= 1;
-		}
-		if (t < 1 / 6) {
-			return p + (q - p) * 6 * t;
-		}
-		if (t < 1 / 2) {
-			return q;
-		}
-		if (t < 2 / 3) {
-			return p + (q - p) * (2 / 3 - t) * 6;
-		}
-		return p;
-	}
-
-	var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-	var p = 2 * l - q;
-
-	if (s === 0) {
-		r = 1;
-		g = 1;
-		b = 1; // achromatic
-	} else {
-
-		r = hue2rgb(p, q, h + 1 / 3);
-		g = hue2rgb(p, q, h);
-		b = hue2rgb(p, q, h - 1 / 3);
-	}
-
-	return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
-}
-
-
-/*
-a.html?x=-0.36163781924926275&y=0.6020825666719246&zoom=549755813888
-*/
-
-function mandelbrot(canvas, math_x, math_y, zoom) {
+function mandelbrot(canvas) {
 	"use strict";
 
-	var im = canvas.getContext("2d").getImageData(0, 0, canvas.width, canvas.height);
-	// var math_x = 0;
-	// var math_y = 0;
-	// var zoom = 1; // max tested 562949953421312
+	// zoom maxes out at about 
+	// 562949953421312
+	// 879609302220800
+	
+	// the zoom itself is just the number that scales the size of the frame
+	// the original size is 4x4 
 
-	function main()
-	{
+	// image and parameters a
+	var im, a;
+
+	function main() {
+		// get image from canvas
+		im = canvas.getContext("2d").getImageData(0, 0, canvas.width, canvas.height);
+
+		// make/get parameters
+		a = { x: 0, y: 0, zoom: 1 } // defaults
+		get_parameters_from_url(a, window.location.href)
+
 		draw_mandelbrot();
-		update_url();
+		update_url_without_refreshing_page();
 		// respond to left and right mouse clicks
 		canvas.addEventListener("mousedown", on_mousedown, false);
 		// disable context menu
-		canvas.addEventListener("contextmenu", (e)=>{ e.preventDefault(); } );
+		canvas.addEventListener("contextmenu", (e) => { e.preventDefault(); });
 
 	}
+
+	function get_parameters_from_url(vars, url) {
+		// from https://html-online.com/articles/get-url-parameters-javascript/
+		var parts = url.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
+			vars[key] = value;
+		});
+		return vars;
+	}
+
+
+	function hslToRgb(h, s, l) {
+		var r;
+		var g;
+		var b;
+		function hue2rgb(p, q, t) {
+			if (t < 0) {
+				t += 1;
+			}
+			if (t > 1) {
+				t -= 1;
+			}
+			if (t < 1 / 6) {
+				return p + (q - p) * 6 * t;
+			}
+			if (t < 1 / 2) {
+				return q;
+			}
+			if (t < 2 / 3) {
+				return p + (q - p) * (2 / 3 - t) * 6;
+			}
+			return p;
+		}
+
+		var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+		var p = 2 * l - q;
+
+		if (s === 0) {
+			r = 1;
+			g = 1;
+			b = 1; // achromatic
+		} else {
+
+			r = hue2rgb(p, q, h + 1 / 3);
+			g = hue2rgb(p, q, h);
+			b = hue2rgb(p, q, h - 1 / 3);
+		}
+
+		return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+	}
+
 
 	function draw_mandelbrot() {
 
@@ -82,19 +98,19 @@ function mandelbrot(canvas, math_x, math_y, zoom) {
 
 				var x, y, zx, zxtemp, zy, cx, cy, rgb, i, in_set, pos;
 				var max_iterations;
-				var math_height = 4 / zoom;
-				var math_width = 4 / zoom;			
+				var math_height = 4 / a.zoom;
+				var math_width = 4 / a.zoom;
 
 				// math numbers
-				cx = (math_x - math_width / 2) + x / (im.width / math_width);
-				cy = (math_y - math_height / 2) + y / (im.width / math_width);
+				cx = (a.x - math_width / 2) + x / (im.width / math_width);
+				cy = (a.y - math_height / 2) + y / (im.width / math_width);
 				// image is flipped vertically. numbers go up, pixels go down
 				// who cares
 				zx = 0;
 				zy = 0;
 
 				in_set = true;
-				max_iterations = (255 + (20 * Math.log2(zoom)));
+				max_iterations = (255 + (20 * Math.log2(a.zoom)));
 				for (i = 1; i <= max_iterations; i += 1) {
 					zxtemp = zx * zx - zy * zy + cx;
 					zy = 2 * zx * zy + cy;
@@ -119,16 +135,9 @@ function mandelbrot(canvas, math_x, math_y, zoom) {
 	}
 
 
-	function update_url() {
-		// consolePara.innerHTML = currentURL(); // URL
-
-		var url = document.location.href
-		if (url.indexOf("?") !== -1) {
-			url = url.slice(0, url.indexOf("?"))
-		}
-		console.log(url + "?x=" + math_x + "&y=" + math_y + "&zoom=" + zoom); //  + "&max_iterations=" + max_iterations);
-		// infoPara.innerHTML = url+"?x="+math_x+"&y="+math_y+"&zoom="+zoom+"&max_iterations="+max_iterations;
-
+	function update_url_without_refreshing_page() {
+		// https://computerrock.com/blog/html5-changing-the-browser-url-without-refreshing-page/
+		window.history.replaceState("object or string", "Title", "?x="+a.x+"&y="+a.y+"&zoom="+a.zoom);
 	}
 
 	var on_mousedown = function (e) {
@@ -138,21 +147,21 @@ function mandelbrot(canvas, math_x, math_y, zoom) {
 			var x_pixel = e.clientX - rect.left;
 			var y_pixel = e.clientY - rect.top;
 
-			var math_height = 4 / zoom;
-			var math_width = 4 / zoom;
-					
-			// update globals
-			math_x = (math_x - math_width / 2) + x_pixel / (im.width / math_width);
-			math_y = (math_y - math_height / 2) + y_pixel / (im.height / math_height);
-			zoom *= 2;
+			var math_height = 4 / a.zoom;
+			var math_width = 4 / a.zoom;
 
-		} else if (e.which === 3) { // right click is 3 for some reason.
+			// update globals
+			a.x = (a.x - math_width / 2) + x_pixel / (im.width / math_width);
+			a.y = (a.y - math_height / 2) + y_pixel / (im.height / math_height);
+			a.zoom *= 2;
+
+		} else if (e.which === 3) { // right click
 			// update global
-			zoom /= 2;
+			a.zoom /= 2;
 		}
 
 		draw_mandelbrot();
-		update_url();
+		update_url_without_refreshing_page();
 	}
 
 	main();
